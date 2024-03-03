9187457cdea3be007b45c5edcc904cf4bdc6b6ba
Veastue <cantigaonduty@gmail.com>
2024-03-03 16:49:14 +0800

    adding meta tags

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
   return (
     <section id='project' className='bg-gray-100 w-full h-full mx-auto flex flex-col items-center py-32'>
       <div className="flex w-full justify-center ">
-        <div className=' sm:w-[300px] lg:w-[1024px] text-xl font-bold text-blue-600 flex px-4'>
+        <div className=' sm:w-[300px] lg:w-[1024px] text-xl text-center font-bold text-blue-600 flex px-4'>
           <h1>Projects</h1>
         </div>
       </div>
